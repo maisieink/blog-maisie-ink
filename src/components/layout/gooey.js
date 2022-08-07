c9f64ba0d5c7a783dd7061e0434360780f049418
maisieink <98289338+maisieink@users.noreply.github.com>
2022-08-07 15:46:52 -0700

    move measurements out of animation frame

--- src/components/layout/gooey.js
+++ src/components/layout/gooey.js
@@ -24,13 +24,9 @@ import classNames from "classnames";
 
 import * as styles from "./gooey.module.css";
 
-const STANDBY = 1;
-const STRETCHING = 2;
-const ANIMATING_COLLAPSE = 3;
-
-const SVG_CIRCLE_RADIUS = 128;
-const STRETCH_ANIMATION_TIME = "100";
-
+/**
+ * Util function for a timeout that can be used with `await`
+ */
 function timeout(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
@@ -40,6 +36,10 @@ function timeout(ms) {
 /**
  * Returns an svg path that draws a circle, with its rightmost node translated
  * by (x, y), which results in drawing a circle stretched to the right.
+ *
+ * Created by exporting a circle path from Inkscape, then exporting versions
+ * with the rightmost node moved right or down, to see which numbers changed.
+ * I don't actually understand the path syntax.
  */
 function getPath(x, y) {
   return `M ${128 + x},${y} C ${128 + x},${
@@ -57,6 +57,209 @@ function getCssPath(inward, sideways) {
 }
 
 /**
+ * <Gooey /> renders an svg of a circle, with the rightmost node of the svg
+ * movable to "stretch" the circle. Gooey follows a state machine from
+ * STANDBY -> STRETCHING -> ANIMATING_COLLAPSE
+ *
+ * In STANDBY, it's just waiting for the mouse cursor to enter and exit the
+ * circle. When leaving the circle, state transitions to STRETCHING. The svg
+ * rotates so that the moveable node is at the position where the cursor
+ * exited the circle.
+ *
+ * In STRETCHING, the moveable node follows the mouse cursor. If the mouse
+ * cursor gets too far from the circle, then the state transitions to
+ * ANIMATING_COLLAPSE.
+ *
+ * In ANIMATING_COLLAPSE, the circle animates back to its original circle
+ * shape. Once the animation is done, state transitions back to STANDBY.
+ *
+ * For performance, we're modifying the DOM nodes directly, and keeping
+ * mutable state inside the useMemo closure. This component does not follow
+ * React conventions.
+ * TODO: Is this actually faster than a setState -> re-render with style prop?
+ */
+const STANDBY = 1,
+  STRETCHING = 2,
+  ANIMATING_COLLAPSE = 3;
+const SVG_CIRCLE_RADIUS = 128;
+const STRETCH_ANIMATION_TIME = 100;
+
+function createGooeyMouseEventHandler(containerRef, pathRef) {
+  // State variables
+  let state = STANDBY;
+  let animationFrame = null;
+  let circle = {
+    clientCenterX: 0,
+    clientCenterY: 0,
+    clientRadius: 1,
+  };
+  let mouseEvent = {
+    current: { clientX: 0, clientY: 0, type: null },
+    prev: { clientX: 0, clientY: 0, type: null },
+  };
+  let rotation = {
+    sin: 0,
+    cos: 1,
+  };
+
+  // Called when the mouse goes too far away from the circle, to collapse the
+  // circle back to its original position.
+  const collapse = async (collapseAnimationTime) => {
+    state = ANIMATING_COLLAPSE;
+
+    // Ensure that the stretching animation finishes before
+    // animating the collapse.
+    await timeout(STRETCH_ANIMATION_TIME);
+
+    requestAnimationFrame(() => {
+      if (pathRef.current) {
+        pathRef.current.style.transition = `d ${collapseAnimationTime}ms cubic-bezier(.6,.22,.47,1.57)`;
+        pathRef.current.style.d = getCssPath(0, 0);
+      }
+    });
+
+    await timeout(collapseAnimationTime);
+
+    state = STANDBY;
+  };
+
+  // Main state machine function
+  const onAnimationFrame = () => {
+    if (pathRef.current && mouseEvent.current.type && mouseEvent.prev.type) {
+      const mouseX = mouseEvent.current.clientX - circle.clientCenterX;
+      const mouseY = mouseEvent.current.clientY - circle.clientCenterY;
+
+      switch (state) {
+        case STANDBY:
+          const prevX = mouseEvent.prev.clientX - circle.clientCenterX;
+          const prevY = mouseEvent.prev.clientY - circle.clientCenterY;
+
+          const circleRadiusSqr = circle.clientRadius ** 2;
+          const mouseRadiusSqr = mouseX ** 2 + mouseY ** 2;
+          const prevMouseRadiusSqr = prevX ** 2 + prevY ** 2;
+
+          // If the mouse moved from within the circle to outside of the
+          // circle, then:
+          // 1) rotate the svg so that the moveable node is where the mouse
+          //    left the circle
+          // 2) transition state machine to STRETCHING
+          if (
+            mouseRadiusSqr >= circleRadiusSqr &&
+            prevMouseRadiusSqr < circleRadiusSqr
+          ) {
+            const mouseRadius = Math.sqrt(mouseRadiusSqr);
+
+            rotation.cos = mouseX / mouseRadius;
+            rotation.sin = mouseY / mouseRadius;
+
+            const { cos, sin } = rotation;
+            const rotationMatrix = `${cos}, ${sin}, ${-sin}, ${cos}, 0, 0`;
+
+            pathRef.current.style.transform = `matrix(${rotationMatrix})`;
+
+            state = STRETCHING;
+          }
+          break;
+
+        case STRETCHING:
+          // Transform the mouse position into the coordinate system of
+          // the rotated, scaled svg. That way, when we move a node within
+          // the svg, it will display where the mouse is.
+
+          // Multiply (x, y) by the inversion of the svg's rotation matrix
+          const rotatedX = mouseX * rotation.cos + mouseY * rotation.sin;
+          const rotatedY = mouseX * -rotation.sin + mouseY * rotation.cos;
+
+          // Scale (x, y) by the radio of display size to svg viewBox
+          const scaledX = rotatedX / (circle.clientRadius / SVG_CIRCLE_RADIUS);
+          const scaledY = rotatedY / (circle.clientRadius / SVG_CIRCLE_RADIUS);
+
+          // Translate the origin from the center of the circle to the
+          // the position of the moveable node on the right side of the
+          // svg.
+          const transformedX = scaledX - SVG_CIRCLE_RADIUS;
+          const transformedY = scaledY;
+
+          /**
+           * Transition to ANIMATING_COLLAPSE if
+           * - The mouse is moved 1/4 radius inward into the circle
+           * - The mouse is moved one radius outside of the circle
+           * - The "sideways" movement is more than the "outward" movement
+           *
+           * Visually, this means the mouse will "drop" the circle when
+           * it's been stretched "too far."
+           */
+          if (
+            mouseEvent.current.type === "mouseleave" ||
+            transformedX < -SVG_CIRCLE_RADIUS / 4 ||
+            transformedX ** 2 + transformedY ** 2 > SVG_CIRCLE_RADIUS ** 2 ||
+            Math.abs(transformedY) >
+              Math.abs(transformedX) + SVG_CIRCLE_RADIUS / 4
+          ) {
+            const collapseAnimationTime =
+              (Math.abs(transformedX) / SVG_CIRCLE_RADIUS) * 500 + 200;
+
+            collapse(collapseAnimationTime);
+          } else {
+            // Otherwise, just update the path so that the moveable node
+            // follows the mouse position.
+            pathRef.current.style.transition = `d ${STRETCH_ANIMATION_TIME}ms`;
+            pathRef.current.style.d = getCssPath(transformedX, transformedY);
+          }
+          break;
+
+        case ANIMATING_COLLAPSE:
+          // Nothing to do in this case, since `state` will transition to
+          // STANDBY after the animation time has passed.
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    mouseEvent.prev.clientX = mouseEvent.current.clientX;
+    mouseEvent.prev.clientY = mouseEvent.current.clientY;
+    mouseEvent.prev.type = mouseEvent.current.type;
+    animationFrame = null;
+  };
+
+  const onMouseEvent = (event) => {
+    if (containerRef.current) {
+      // Because clientX, clientY and getBoundingClientRect can trigger reflow,
+      // measure them now, before any layout changes have happened in the
+      // animation frame. For example, if we have two Gooeys render on the
+      // page, we want this behavior:
+      //
+      // Gooey #1 mouseEvent: measure
+      // Gooey #2 mouseEvent: measure <-- does not trigger reflow since
+      //                                  Gooey #1 waited until animation
+      //                                  frame to make changes
+      // Gooey #1 animationFrame: make changes
+      // Gooey #2 animationFrame: make changes
+      //
+      // Might not matter in this specific case, since we're not modifying any
+      // styles that would affect layout, but it could if there are other
+      // components in the app performing similar DOM modifications.
+      mouseEvent.current.clientX = event.clientX;
+      mouseEvent.current.clientY = event.clientY;
+      mouseEvent.current.type = event.type;
+
+      const rect = containerRef.current.getBoundingClientRect();
+      circle.clientCenterX = (rect.left + rect.right) / 2;
+      circle.clientCenterY = (rect.top + rect.bottom) / 2;
+      circle.clientRadius = rect.width / 4;
+
+      if (animationFrame === null) {
+        animationFrame = requestAnimationFrame(onAnimationFrame);
+      }
+    }
+  };
+
+  return onMouseEvent;
+}
+
+/**
  * Renders a circle that will move like goo when you move the mouse in and out
  * of it.
  *
@@ -68,192 +271,19 @@ function getCssPath(inward, sideways) {
 const Gooey = ({ color, addMouseHandler, removeMouseHandler }) => {
   const containerRef = useRef();
   const pathRef = useRef();
-
-  /**
-   * Gooey renders an svg of a circle, with the rightmost node of the svg
-   * movable to "stretch" the circle. Gooey follows a state machine from
-   * STANDBY -> STRETCHING -> ANIMATING_COLLAPSE
-   *
-   * In STANDBY, it's just waiting for the mouse cursor to enter and exit the
-   * circle. When leaving the circle, state transitions to STRETCHING. The svg
-   * rotates so that the moveable node is at the position where the cursor
-   * exited the circle.
-   *
-   * In STRETCHING, the moveable node follows the mouse cursor. If the mouse
-   * cursor gets too far from the circle, then the state transitions to
-   * ANIMATING_COLLAPSE.
-   *
-   * In ANIMATING_COLLAPSE, the circle animates back to its original circle
-   * shape. Once the animation is done, state transitions back to STANDBY.
-   *
-   * For performance, we're modifying the DOM nodes directly, and keeping
-   * mutable state inside the useMemo closure. This component does not follow
-   * React conventions. TODO: Move out of useMemo and into a ref, since useMemo
-   * isn't guaranteed to only run once. TODO: Is this actually faster than a
-   * setState -> re-render with style attribute?
-   *
-   * I could probably split this up some more into multiple functions to make
-   * it more readable (with function names instead of comments), but this is
-   * good enough for now.
-   */
-  const onMouseEvent = useMemo(() => {
-    let state = STANDBY;
-    let animationFrame = null;
-    let mouseEvent = {
-      current: null,
-      prev: null,
-    };
-    let rotation = {
-      sin: 0,
-      cos: 1,
-    };
-
-    const onAnimationFrame = () => {
-      if (
-        containerRef.current &&
-        pathRef.current &&
-        mouseEvent.current &&
-        mouseEvent.prev
-      ) {
-        // TODO: Do perf testing to make sure that this doesn't reflow if nothing
-        // has changed since the last call? TODO: Move this measurement in
-        // the the event handler instead of animation frame to make layout thrashing less likely
-        const rect = containerRef.current.getBoundingClientRect();
-        const centerX = (rect.left + rect.right) / 2 + window.scrollX;
-        const centerY = (rect.top + rect.bottom) / 2 + window.scrollY;
-        const circleRadius = rect.width / 4;
-
-        // TODO: use clientX/Y to avoid adding scrollX/scrollY
-        const mouseX = mouseEvent.current.pageX - centerX;
-        const mouseY = mouseEvent.current.pageY - centerY;
-
-        switch (state) {
-          case STANDBY:
-            const prevX = mouseEvent.prev.pageX - centerX;
-            const prevY = mouseEvent.prev.pageY - centerY;
-
-            const circleRadiusSqr = circleRadius ** 2;
-            const mouseRadiusSqr = mouseX ** 2 + mouseY ** 2;
-            const prevMouseRadiusSqr = prevX ** 2 + prevY ** 2;
-
-            // If the mouse moved from within the circle to outside of the
-            // circle, then:
-            // 1) rotate the svg so that the moveable node is where the mouse
-            //    left the circle
-            // 2) transition state machine to STRETCHING
-            if (
-              mouseRadiusSqr >= circleRadiusSqr &&
-              prevMouseRadiusSqr < circleRadiusSqr
-            ) {
-              const mouseRadius = Math.sqrt(mouseRadiusSqr);
-
-              rotation.cos = mouseX / mouseRadius;
-              rotation.sin = mouseY / mouseRadius;
-
-              const { cos, sin } = rotation;
-              const rotationMatrix = `${cos}, ${sin}, ${-sin}, ${cos}, 0, 0`;
-
-              pathRef.current.style.transform = `matrix(${rotationMatrix})`;
-
-              state = STRETCHING;
-            }
-            break;
-
-          case STRETCHING:
-            // Transform the mouse position into the coordinate system of
-            // the rotated, scaled svg. That way, when we move a node within
-            // the svg, it will display where the mouse is.
-
-            // Multiply (x, y) by the inversion of the svg's rotation matrix
-            const rotatedX = mouseX * rotation.cos + mouseY * rotation.sin;
-            const rotatedY = mouseX * -rotation.sin + mouseY * rotation.cos;
-
-            // Scale (x, y) by the radio of display size to svg viewBox
-            const scaledX = rotatedX / (circleRadius / SVG_CIRCLE_RADIUS);
-            const scaledY = rotatedY / (circleRadius / SVG_CIRCLE_RADIUS);
-
-            // Translate the origin from the center of the circle to the
-            // the position of the moveable node on the right side of the
-            // svg.
-            const transformedX = scaledX - SVG_CIRCLE_RADIUS;
-            const transformedY = scaledY;
-
-            /**
-             * Transition to ANIMATING_COLLAPSE if
-             * - The mouse is moved 1/4 radius inward into the circle
-             * - The mouse is moved one radius outside of the circle
-             * - The "sideways" movement is more than the "outward" movement
-             *
-             * Visually, this means the mouse will "drop" the circle when
-             * it's been stretched "too far."
-             */
-            if (
-              // TODO: Just call collapse directly on mouseleave with a separate
-              // handler?
-              mouseEvent.current.type === "mouseleave" ||
-              transformedX < -SVG_CIRCLE_RADIUS / 4 ||
-              transformedX ** 2 + transformedY ** 2 > SVG_CIRCLE_RADIUS ** 2 ||
-              Math.abs(transformedY) >
-                Math.abs(transformedX) + SVG_CIRCLE_RADIUS / 4
-            ) {
-              state = ANIMATING_COLLAPSE;
-
-              const collapse = async () => {
-                const collapseAnimationTime =
-                  (Math.abs(transformedX) / SVG_CIRCLE_RADIUS) * 500 + 200;
-
-                // Ensure that the stretching animation finishes before
-                // animating the collapse.
-                await timeout(STRETCH_ANIMATION_TIME);
-
-                // TODO: request animation frame here
-                if (pathRef.current) {
-                  pathRef.current.style.transition = `d ${collapseAnimationTime}ms cubic-bezier(.6,.22,.47,1.57)`;
-                  pathRef.current.style.d = getCssPath(0, 0);
-                }
-
-                await timeout(collapseAnimationTime);
-
-                state = STANDBY;
-              };
-              collapse();
-            } else {
-              // Otherwise, just update the path so that the moveable node
-              // follows the mouse position.
-              pathRef.current.style.transition = `d ${STRETCH_ANIMATION_TIME}ms`;
-              pathRef.current.style.d = getCssPath(transformedX, transformedY);
-            }
-            break;
-
-          case ANIMATING_COLLAPSE:
-            // Nothing to do in this case, since `state` will transition to
-            // STANDBY after the animation time has passed.
-            break;
-
-          default:
-            break;
-        }
-      }
-
-      mouseEvent.prev = mouseEvent.current;
-      animationFrame = null;
-    };
-
-    const onMouseEventClosure = (event) => {
-      mouseEvent.current = event;
-
-      if (animationFrame === null) {
-        animationFrame = requestAnimationFrame(onAnimationFrame);
-      }
-    };
-
-    return onMouseEventClosure;
-  }, []);
+  const onMouseEvent = useRef();
 
   useEffect(() => {
-    addMouseHandler(onMouseEvent);
-    return () => removeMouseHandler(onMouseEvent);
-  }, [onMouseEvent, addMouseHandler, removeMouseHandler]);
+    if (!onMouseEvent.current) {
+      onMouseEvent.current = createGooeyMouseEventHandler(
+        containerRef,
+        pathRef
+      );
+    }
+
+    addMouseHandler(onMouseEvent.current);
+    return () => removeMouseHandler(onMouseEvent.current);
+  }, [addMouseHandler, removeMouseHandler]);
 
   const basePath = useMemo(() => getPath(0, 0), []);
 
